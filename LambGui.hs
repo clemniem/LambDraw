5f4397699ac10c7918073470865258681f4a327b
clemniem <clemens.niemeyer@gmail.com>
2015-02-20 14:17:18 +0100

    RGB Filler implemented

--- LambGui.hs
+++ LambGui.hs
@@ -18,29 +18,76 @@ setup window = do
 
 
     canvas <- UI.canvas
-        # set UI.height 400
-        # set UI.width  400
+        # set UI.height 35
+        # set UI.width  35
         # set style [("border", "solid black 1px")]
     imgur  <- UI.image
-        # set UI.height 400
-        # set UI.width  400
+        # set UI.height 300
+        # set UI.width  300
         # set style [("border", "solid black 1px")]
         # set UI.src "static/canvas.png"
+    imgur2  <- UI.image
+        # set UI.height 300
+        # set UI.width  300
+        # set style [("border", "solid black 1px")]
+        # set UI.src "static/t2.png"
     drawWidth   <- UI.input -- dw
     drawHeight  <- UI.input -- dh
     applyResize <- UI.button #+ [string "Apply new Size."]
     
+    rVal <- UI.input
+    gVal <- UI.input
+    bVal <- UI.input
+
+    addColor    <- UI.button #+ [string "Add Color to Palette     "]
+    removeColor <- UI.button #+ [string "Remove Color from Palette"]
+
+    colPick <- UI.div #+ [grid [[string "R",element rVal], [string "G", element gVal],[string "B", element bVal]]
+                         ,row [element canvas, column [element addColor, element removeColor]]
+                         ]
+
+        # set UI.height 300
+        # set UI.width  300
+        # set style [("left", "50 px")]
+        # set UI.align "top"
+        # set UI.valign "left"
+
+
+
+
     addRects <- UI.button #+ [string "Add some rectangles."]
     addArcs  <- UI.button #+ [string "Add some arcs and circles."]
     clear    <- UI.button #+ [string "Clear the canvas."]
 
     getBody window #+ [
-        row [element imgur, element canvas]
-        ,grid [[string "DrawWidth :" , element drawWidth  ]
-              ,[string "drawHeight:" , element drawHeight ]
-              ,[element applyResize]]
-        , element addRects, element addArcs, element clear
+        element colPick, row [element imgur, element imgur2]
         ]
+
+        -- row [column [ element imgur,
+        --        row  [ grid [[string "Red", element rVal]
+        --               ,[string "Green",element gVal]
+        --               ,[string "Blue",element bVal]]]
+        --             ,element canvas]] 
+        -- ,grid [[string "DrawWidth :" , element drawWidth  ]
+        --       ,[string "drawHeight:" , element drawHeight ]
+        --       ,[element applyResize]]
+        -- , element addRects, element addArcs, element clear
+
+    bRIn <- stepper "0" $ UI.valueChange rVal
+    bGIn <- stepper "0" $ UI.valueChange gVal
+    bBIn <- stepper "0" $ UI.valueChange bVal
+
+    on UI.click addColor $ const $ do
+        rIn  <- currentValue bRIn
+        gIn  <- currentValue bGIn
+        bIn  <- currentValue bBIn
+        element canvas # set UI.fillStyle (UI.solidColor $ UI.RGB (read rIn :: Int) 
+                                                                  (read gIn :: Int) 
+                                                                  (read bIn :: Int))
+        UI.fillRect (0,0) 35 35 canvas
+        --liftIOLater $ putStrLn rIn
+
+    on UI.click removeColor $ const $ UI.clearCanvas canvas
 
     dwIn   <- stepper "0" $ UI.valueChange drawWidth
     on UI.click applyResize $ const $ element drawHeight # sink value dwIn
@@ -49,7 +96,7 @@ setup window = do
           return $ liftIOLater $ processImage 1 "./images/canvas.png" "./images/tempDith"
           
 
-    onEvent (UI.click applyResize) procImg
+    on UI.click applyResize procImg
     on UI.click applyResize $ return $ element imgur # set UI.src "static/tempDith.png"
 
 
